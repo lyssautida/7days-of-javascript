e64454e818895807541fdb0d53f0f2ac2784baf8
Lyssa <lyssamailbox@gmail.com>
2022-04-13 19:52:57 -0400

    ✨new challenge day - 4th finding the secret number

---
+++ day-four/day-four.js
@@ -0,0 +1,9 @@
+let secret = (10);
+let guess = prompt("Adivinhe o número secreto: ");
+let victoryMessage = `Você acertou o número secreto ${secret}. Parabéns!`;
+const lossMessage = `Você errou o número secreto. Tente novamente!`;
+if (guess == 10) {
+    alert(victoryMessage);
+} else {
+alert(lossMessage)
+}
